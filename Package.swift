505386bdd40778f24c2933ea2e8d0ab01e20751a
Build Er <builder@macmini6.local>
2023-05-31 02:09:04 -0300

    Package version: 1.0.0-beta+20230531050040

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCCharts",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCCharts-1.0.0-beta+20230526182522.xcframework.zip",
-			checksum: "d294c5da37227259ad3f237c6d79c59a43096097814ba848f2e526d0b7955a8b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCCharts-1.0.0-beta+20230531050040.xcframework.zip",
+			checksum: "8487400086bb93b081003b5682dae187fcc810ccfd6c5736dd4d903dc410ef03"
 		)
 	]
 )
