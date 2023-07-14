6ba8cfc5bc6b5abfad7631a6b22104fe890689db
genexusbot <builder@genexus.com>
2023-07-14 16:30:46 -0300

    Package version: 1.0.0-preview.20230714192150

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXUCCharts",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXUCCharts",
+			targets: ["GXUCChartsWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.0.0-preview.20230714192150")
+	],
+	targets: [
+		.target(name: "GXUCChartsWrapper",
+				dependencies: [
+					"GXUCCharts",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXUCCharts",
+			url: "https://pkgs.genexus.dev/iOS/preview/GXUCCharts-1.0.0-preview.20230714192150.xcframework.zip",
+			checksum: "dece0971f30e7e4e44064ee53b4725b4e474b2954fa665104894aea23e08f271"
+		)
+	]
+)
