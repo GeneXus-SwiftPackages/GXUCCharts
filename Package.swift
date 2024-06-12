a399d2edb862be8c27710eb789ee76352c6fdaeb
Build Er <builder@macmini6.local>
2024-06-12 14:21:51 -0300

    Package version: 2.0.12

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -10,23 +10,19 @@ let package = Package(
 			targets: ["GXUCChartsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.12")
 	],
 	targets: [
 		.target(name: "GXUCChartsWrapper",
 				dependencies: [
 					"GXUCCharts",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
-					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
-					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCCharts",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXUCCharts-1.1.0.xcframework.zip",
-			checksum: "b93b0fb5671b8283856aa454c511a5668bb782fb65d52b5c9030ae314e5b3033"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXUCCharts-2.0.12.xcframework.zip",
+			checksum: "d834fb99c80e2451eb5292679cf275ad98a1a7996e9fc0735b25dc3cec0a1e9f"
 		)
 	]
 )
