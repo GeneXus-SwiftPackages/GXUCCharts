4ef14982e4ae7814aee5ed27a8f541ac5ebeb0f9
The iOS Team @ GeneXus <builder@genexus.com>
2026-08-10 19:49:54 -0400

    Package version: 5.0.0-beta.4

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUCChartsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "5.0.0-beta.1")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "5.0.0-beta.4")
 	],
 	targets: [
 		.target(name: "GXUCChartsWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCCharts",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCCharts-5.0.0-beta.1.xcframework.zip",
-			checksum: "713607014b570ca9c695a4346b3f3efd9dafaeb90502f159df658311bc505644"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCCharts-5.0.0-beta.4.xcframework.zip",
+			checksum: "47e3784110512e8efdc0d487655c247ce785cc86e14dd418a488605a08f13758"
 		)
 	]
 )
